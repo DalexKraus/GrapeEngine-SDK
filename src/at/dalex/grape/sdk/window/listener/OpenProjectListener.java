666a56051ca45a54232b2bb7a0bb8b53593e7720
DalexKraus <dalex.kraus@gmail.com>
2019-05-02 09:43:02 +0200

    Fixed project validation

---
+++ src/at/dalex/grape/sdk/window/listener/OpenProjectListener.java
@@ -0,0 +1,35 @@
+package at.dalex.grape.sdk.window.listener;
+
+import at.dalex.grape.sdk.project.ProjectUtil;
+import at.dalex.grape.sdk.window.helper.DialogHelper;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class OpenProjectListener implements EventHandler<ActionEvent> {
+
+    private Stage anchor;
+    public OpenProjectListener(Stage anchor) {
+        this.anchor = anchor;
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setInitialDirectory(ProjectUtil.getDefaultProjectDirectory());
+        File selectedFolder = chooser.showDialog(anchor);
+        if (selectedFolder == null)
+            return;
+
+        //Validate selected folder
+        File projectFile = new File(selectedFolder.getAbsolutePath() + "/.sdk/project.json");
+        if (!projectFile.exists())
+            DialogHelper.showErrorDialog("Error", null, "The selected folder does not seem to contain " +
+                                                        "a valid project!");
+
+
+    }
+}
