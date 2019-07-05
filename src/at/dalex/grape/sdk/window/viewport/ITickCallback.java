a368de80c173307848b8c994f8bacbba0a88ed96
Dalex <Dalex@DESKTOP-C0SOJHB>
2019-07-05 19:39:13 +0200

    Documented ITickCallback

--- src/at/dalex/grape/sdk/window/viewport/ITickCallback.java
+++ src/at/dalex/grape/sdk/window/viewport/ITickCallback.java
@@ -2,6 +2,13 @@ package at.dalex.grape.sdk.window.viewport;
 
 import javafx.scene.canvas.GraphicsContext;
 
+/**
+ * This interface acts as a callback for the {@link ViewportCanvas}.
+ * The methods get called when a new frame is about to be rendered.
+ *
+ * The deriving class can then use those callbacks to draw their own
+ * stuff on the {@link ViewportCanvas}
+ */
 public interface ITickCallback {
 
     void update(double delta);
