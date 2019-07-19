fe3f119ded91522a2aba7c9e01def91f5e4398ac
DalexKraus <dalex.kraus@gmail.com>
2019-07-19 14:38:03 +0200

    Added stuff to the property split pane

--- src/at/dalex/grape/sdk/window/propertypanel/MapPropertyPanel.java
+++ src/at/dalex/grape/sdk/window/propertypanel/MapPropertyPanel.java
@@ -1,22 +1,33 @@
 package at.dalex.grape.sdk.window.propertypanel;
 
+import javafx.geometry.Orientation;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TitledPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.Pane;
 
 public class MapPropertyPanel extends TitledPane {
 
     private static final String TAB_TITLE = "Properties";
-    private VBox contentPanel;
+    private SplitPane splitPane;
+    private TreeView nodeTree;
 
     public MapPropertyPanel() {
-        /* Set the title and add a panel for content (vertical layout) */
+        /* Set the title and add a split-pane for content (vertical orientation) */
         setText(TAB_TITLE);
-        this.contentPanel = new VBox();
-        setContent(contentPanel);
+        this.splitPane = new SplitPane();
+        splitPane.setOrientation(Orientation.VERTICAL);
+        setContent(splitPane);
         setCollapsible(false);
 
         /* Set preferred dimensions */
         setPrefHeight(Double.MAX_VALUE);
+
+        /* Create Node-Tree */
+        this.nodeTree = new TreeView();
+        splitPane.getItems().add(nodeTree);
+        splitPane.getItems().add(new Pane());
+        splitPane.setDividerPosition(0, 0.5f);
     }
 
 }
