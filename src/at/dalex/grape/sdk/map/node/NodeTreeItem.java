dbc530ee1e8e6f93ec1dc62fb598ab41e0f5bf30
Dalex <Dalex@DESKTOP-C0SOJHB>
2019-07-30 14:48:08 +0200

    Added NodeTreeItem

---
+++ src/at/dalex/grape/sdk/map/node/NodeTreeItem.java
@@ -0,0 +1,33 @@
+package at.dalex.grape.sdk.map.node;
+
+import javafx.scene.control.TreeItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class NodeTreeItem extends TreeItem<NodeBase> {
+
+    public NodeTreeItem(NodeBase node, ImageView iconImage) {
+        super(node, iconImage);
+    }
+
+    /**
+     * Refreshes the children of this item.
+     */
+    public void refreshChildren() {
+        NodeBase node = getValue();
+        if (node == null)
+            return;
+
+        for (NodeBase child : node.getChildren()) {
+            String nodeTitle = node.getTitle();
+            Image nodeIcon = node.getTreeIcon();
+
+            super.getChildren().add(new NodeTreeItem(node, new ImageView(nodeIcon)));
+        }
+    }
+
+    @Override
+    public boolean isLeaf() {
+        return super.isLeaf();
+    }
+}
