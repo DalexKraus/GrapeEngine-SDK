39bf969d079598570e69f5862022942dab2bd625
DalexKraus <dalex.kraus@gmail.com>
2019-07-30 23:10:53 +0200

    Documented Scene class

--- src/at/dalex/grape/sdk/scene/Scene.java
+++ src/at/dalex/grape/sdk/scene/Scene.java
@@ -2,10 +2,17 @@ package at.dalex.grape.sdk.scene;
 
 import at.dalex.grape.sdk.scene.node.RootNode;
 
-public class Scene {
+import java.io.Serializable;
+
+public class Scene implements Serializable {
 
     private String name;
     private RootNode rootNode;
+
+    public Scene(String name) {
+        this.name = name;
+        this.rootNode = new RootNode();
+    }
 
     public String getName() {
         return name;
