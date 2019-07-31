b0c8546e3cd6ef94b99481e9e4740f709d299e9e
DalexKraus <dalex.kraus@gmail.com>
2019-07-31 23:14:43 +0200

    Changed About Dialog

--- src/at/dalex/grape/sdk/window/listener/HelpAboutListener.java
+++ src/at/dalex/grape/sdk/window/listener/HelpAboutListener.java
@@ -1,5 +1,6 @@
 package at.dalex.grape.sdk.window.listener;
 
+import at.dalex.grape.sdk.Main;
 import at.dalex.grape.sdk.window.helper.DialogHelper;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -8,7 +9,11 @@ public class HelpAboutListener implements EventHandler<ActionEvent> {
 
     @Override
     public void handle(ActionEvent event) {
-        DialogHelper.showInformationDialog("About", "Credits", "Main developer: David Kraus\n" +
+        DialogHelper.showInformationDialog("About", "GrapeEngine Software Development Kit (Version " + Main.VERSION + ")",
+                                                    "Main developer: David Kraus\n\n" +
+                                                    "Additional developers:\n" +
+                                                    "Samuel Kowatschek\n" +
+                                                    "Patrick Huemer\n" +
                                                     "\n" +
                                                     "© 2019 David Kraus. All rights reserved.");
     }
