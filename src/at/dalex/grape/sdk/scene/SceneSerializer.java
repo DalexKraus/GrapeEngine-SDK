0ec20ce831aa30cad7c05e9c81fd2eedd45b50da
DalexKraus <dalex.kraus@gmail.com>
2020-02-03 17:47:00 +0100

    Updated SceneSerializer

--- src/at/dalex/grape/sdk/scene/SceneSerializer.java
+++ src/at/dalex/grape/sdk/scene/SceneSerializer.java
@@ -1,21 +1,64 @@
 package at.dalex.grape.sdk.scene;
 
 import at.dalex.grape.sdk.scene.node.NodeBase;
+import at.dalex.grape.sdk.window.helper.DialogHelper;
+import at.dalex.util.JSONUtil;
 import at.dalex.util.math.Vector2f;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class SceneSerializer {
 
-    public void writeScene(Scene scene, File destination) {
+    /**
+     * Serializes the scene object.
+     * The result is a json file.
+     *
+     * @param scene The scene to serialize-
+     * @param fqDestination The fully qualified destination path of the output file.
+     */
+    public static void writeScene(Scene scene, File fqDestination) {
         JSONObject root = new JSONObject();
-        root.put("Name", scene.getName());
+        /* Store the scene name*/
+        root.put("SceneName", scene.getName());
+        /* Store the root node id */
+        UUID rootNodeId = scene.getRootNode().getId();
+        root.put("RootNodeId", rootNodeId.toString());
 
+        /* Store nodes */
+        JSONObject nodes = new JSONObject();
+        HashMap<UUID, JSONObject> nodeObjects = new HashMap<>();
+        collectNodeObjects(nodeObjects, scene.getRootNode());
+        for (UUID nodeId : nodeObjects.keySet()) {
+            nodes.put(nodeId, nodeObjects.get(nodeId));
+        }
+        root.put("Nodes", nodes);
 
+        try (FileWriter file = new FileWriter(fqDestination)) {
+            file.write(JSONUtil.prettyPrintJSON(root.toJSONString()));
+            file.flush();
+        } catch (IOException e) {
+            DialogHelper.showErrorDialog("Error", "Write Error", "The scene could not be written to disk.\n" +
+                    "Target destination: " + fqDestination.getAbsolutePath() + "\n\n" +
+                    "Please check read and write permissions.");
+            e.printStackTrace();
+        }
     }
 
-    private JSONObject createNodeObject(NodeBase nodeInstance) {
+    private static void collectNodeObjects(HashMap<UUID, JSONObject> collection, NodeBase rootNode) {
+        for (NodeBase childNode : rootNode.getChildren()) {
+            collection.put(childNode.getId(), createNodeObject(childNode));
+            collectNodeObjects(collection, childNode);
+        }
+    }
+
+    private static JSONObject createNodeObject(NodeBase nodeInstance) {
         JSONObject node = new JSONObject();
         /* Save node class */
         node.put("NodeClass", nodeInstance.getClass().getName());
@@ -27,6 +70,13 @@ public class SceneSerializer {
         position.put("y", parentSpaceLocation.getY());
         node.put("ParentSpaceLocation", position);
 
+        /* Save child ids */
+        ArrayList<NodeBase> nodeChildren = nodeInstance.getChildren();
+        JSONArray childrenIds = new JSONArray();
+        for (NodeBase nodeChild : nodeChildren) {
+            childrenIds.add(nodeChild.getId());
+        }
+        node.put("Children", childrenIds);
 
         return node;
     }
